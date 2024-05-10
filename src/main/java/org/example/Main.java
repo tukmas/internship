7fa67baf0584365ff01aeae7ed596477cdf564c9
Sergey <tukmachev.18@yandex.ru>
2024-05-10 20:52:24 +0400

    ДЗ 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,35 +12,49 @@ public class Main {
 
         List<City> cities = new ArrayList<>();
 
-            try {
-                File file = new File(String.valueOf(filePath));
-                Scanner scanner = new Scanner(file);
+        try {
+            File file = new File(String.valueOf(filePath));
+            Scanner scanner = new Scanner(file);
 
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    String[] cityData = line.split(";");
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] cityData = line.split(";");
 
-                    if (cityData.length == 6) {
-                        int id = Integer.parseInt(cityData[0]);
-                        String name = cityData[1];
-                        String region = cityData[2];
-                        String district = cityData[3];
-                        int population = Integer.parseInt(cityData[4]);
-                        int foundationYear = parseFoundationYear(cityData[5]);
-
-                        City city = new City(id,name, region, district, population, foundationYear);
-                        System.out.println(city);
-                        id++;
-                    }
+                if (cityData.length == 6) {
+                    int id = Integer.parseInt(cityData[0]);
+                    String name = cityData[1];
+                    String region = cityData[2];
+                    String district = cityData[3];
+                    int population = Integer.parseInt(cityData[4]);
+                    int foundationYear = parseFoundationYear(cityData[5]);
+                    City city = new City(id,name, region, district, population, foundationYear);
+                    cities.add(city);
+                    id++;
                 }
+            }
 
-                scanner.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+
+//        Сортировка списка городов по федеральному округу и наименованию города внутри каждого федерального округа в алфавитном порядке по убыванию с учетом регистра;
+        Comparator<City> comparator = Comparator.comparing(City::getDistrict)
+                .thenComparing(City::getName);
+        cities.sort(comparator);
+
         for (City city : cities) {
             System.out.println(city);
         }
+
+// Сортировка списка городов по наименованию в алфавитном порядке по убыванию без учета регистра;
+//        Collections.sort(cities, (city1, city2) ->
+//                city1.getName().compareToIgnoreCase(city2.getName()));
+//
+//        for (City city : cities) {
+//            System.out.println(city);
+//        }
+
     }
     private static int parseFoundationYear(String yearString) {
         int foundationYear = 0;
